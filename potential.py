a3996c61f0ecfe13557c95c7289b80f4be4427bb
dransom90 <dransom90@yahoo.com>
2020-09-25 07:51:48 -0600

    Move Potential Score Logic
    Moved the potential score logic to its proper home.

--- potential.py
+++ potential.py
@@ -11,16 +11,16 @@ print("POTENTIAL SCORE CALCULATOR")
 year = int(sys.argv[1])
 week = int(sys.argv[2])
 
-print("Retrieving data from ESPN")
+print("\nRetrieving data from ESPN")
 ff_stats = Stats(1525510, year)
 
 teams = ["Hey Baby Let's Go to Vegas", "LA Broncos", "The Chizwit", "how 'bout them Cowboys", "Dos Equis", "cant stop the dopp", "Cobra Kai", "pirate  angel", "Snickle Fritz", "Discount  Belichick"]
 
 for x in teams:
-	print(x)
+	print("\n" + x)
 
-	print("Calculating...")
+	print("\tCalculating...")
 	potential = ff_stats.get_team_potential(x, week)
 
-	print("Updating...")
+	print("\tUpdating...")
 	ff_stats.update_team_potential(x, potential, week)
